d8fa1d0dcc8547b63322dd1a410fcb50ee25f8f8
Strong <Strong.ali.1713@gmail.com>
2020-05-30 21:05:44 +0300

    first practic lesons

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-"use sctrict";
+"use strict";
 
-let name = prompt('Ваше имя?','');
+let numberOfFilms = +prompt("Скільки фільмів ви вже переглянули?","");
 
-alert(`Pruvit, ${name}`);
+let personalMovieDB = {
 
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+
+};
+
+let lastFilm = prompt("Один із останіх переглянутих фільмів?","");
+let assessment = prompt("На скільки оціните його?","");
+
+personalMovieDB.movies[lastFilm] = assessment;
+
+console.log(personalMovieDB);
